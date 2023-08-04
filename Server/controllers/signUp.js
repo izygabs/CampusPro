a8725ed56f4c1690b279e5a1c3dc99e7ed7451f6
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-08-04 19:10:39 +0100

    update on signup

--- Server/controllers/signUp.js
+++ Server/controllers/signUp.js
@@ -3,49 +3,55 @@ const { user } = require("../models/userSchema");
 const validator = require("../validators/joiValidation");
 const { StatusCodes } = require("http-status-codes");
 const errorHandler = require("../middlewares/handleError");
+const fs = require("fs");
 
 const signUp = async (req, res) => {
   const { error, value } = validator.signUp(req.body);
-  console.log(value);
-  if (req.file == null) {
+  let file = req.file;
+  if (file == null) {
     res
       .status(StatusCodes.BAD_REQUEST)
       .json({ "Image Error": "Upload profile picture" });
-  }
-  if (error) {
-    const errors = errorHandler.JoiErrorHandler(error);
-    res.status(StatusCodes.NOT_ACCEPTABLE).json({ error: errors });
   } else {
-    try {
-      const userExist = await user.findOne({ email: value.Email });
-      if (userExist) {
-        res.status(StatusCodes.CONFLICT).send("User Exist");
-      } else {
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(value.Password, salt);
-        const agents = new user({
-          firstName: value.firstName,
-          lastName: value.lastName,
-          email: value.Email,
-          phoneNumber: value.Phone,
-          altPhoneNumber: value.altPhoneNumber || "",
-          password: hashedPassword,
-          profilePic: req.file.path,
-          typeOfUser: value.typeOfUser,
-          propertyIds: [],
-          itemIds: [],
-        });
-        let newUser = await agents.save();
+    if (error) {
+      file && fs.unlinkSync(file.path); // delete image saved in the images folder if joi validation failed
+
+      const errors = errorHandler.JoiErrorHandler(error);
+      res.status(StatusCodes.NOT_ACCEPTABLE).json({ error: errors });
+    } else {
+      try {
+        const userExist = await user.findOne({ email: value.Email });
+        if (userExist) {
+          res.status(StatusCodes.CONFLICT).send("User Exist");
+        } else {
+          const salt = await bcrypt.genSalt();
+          const hashedPassword = await bcrypt.hash(value.Password, salt);
+          const agents = new user({
+            firstName: value.firstName,
+            lastName: value.lastName,
+            email: value.Email.toLowerCase(),
+            phoneNumber: value.Phone,
+            altPhoneNumber: value.altPhoneNumber || "",
+            password: hashedPassword,
+            profilePic: req.file.path,
+            typeOfUser: value.typeOfUser,
+            propertyIds: [],
+            itemIds: [],
+          });
+          let newUser = await agents.save();
+          res
+            .status(StatusCodes.CREATED)
+            .json({ "Account created successfully": newUser });
+        }
+      } catch (error) {
+        console.log(error);
+        file && fs.unlinkSync(file.path); // delete image saved in the images folder if no new user created
+
+        const errors = errorHandler.dbSchemaErrors(error);
         res
-          .status(StatusCodes.CREATED)
-          .json({ "Account created successfully": newUser });
+          .status(StatusCodes.EXPECTATION_FAILED)
+          .json({ "Error message": errors });
       }
-    } catch (error) {
-      console.log(error);
-      const errors = errorHandler.dbSchemaErrors(error);
-      res
-        .status(StatusCodes.EXPECTATION_FAILED)
-        .json({ "Error message": errors });
     }
   }
 };
--- Server/validators/uploadFile.js
+++ Server/validators/uploadFile.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./Server/images");
+    cb(null, "./images");
   },
 
   filename: function (req, file, cb) {
