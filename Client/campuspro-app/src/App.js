d65fa8fefd7c0721f737e814c5503ca55c0becc3
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-08-08 19:50:59 +0100

    Add login and dashboard

--- Client/campuspro-app/src/App.js
+++ Client/campuspro-app/src/App.js
@@ -1,22 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import css from './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import css from "./App.css";
 import HomePage from "./homePage.js";
-import RentPage1 from './rentPage1';
-import RentPage2 from './rentPage2';
-import BuyPage from './buyPage1';
+import RentPage1 from "./rentPage1";
+import RentPage2 from "./rentPage2";
+import BuyPage from "./buyPage1";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dashboard";
+
 function App() {
   return (
-      <div className="App">
-    <Router>
+    <div className="App">
+      <Login />
+      <Dashboard />
+      {/* <Router>
         <Routes>
           <Route path='/' element={<HomePage/>}/>
           <Route path="/01-rentpage" element={<RentPage1/>}/>
           <Route path="/02-rentpage" element={<RentPage2/>}/>
           <Route path="/buyPage1" element={<BuyPage/>}/>
         </Routes>
-    </Router>
+    </Router> */}
     </div>
   );
-}                                                                                                                                        
+}
 
 export default App;
