dacd7370caeea451e218c51331c21a848eac1385
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-08-18 23:40:36 +0100

    update on profile and change password

--- Server/controllers/userProfile.js
+++ Server/controllers/userProfile.js
@@ -1,12 +1,14 @@
 const { user } = require("../models/userSchema");
 
 const userProfile = async (req, res) => {
-  const userId = req.user;
+  const userId = req.params.id;
+  console.log(userId);
   try {
     const profile = await user.findById(
       { _id: userId },
       "firstName lastName profilePic phoneNumber email"
     );
+    console.log(profile);
     res.status(200).json({ Profile: profile });
   } catch (error) {
     console.log("Error in getting the users", error);
