5943763ed0d4e9e97f9700e2ae833763e0c01bb9
k2lawbaaba <kolawole.abiodun2016@gmail.com>
2023-08-21 11:12:50 +0100

    create Properties tray and card

--- Client/campuspro-app/src/Components/PropertytTray.js
+++ Client/campuspro-app/src/Components/PropertytTray.js
@@ -1,4 +1,24 @@
 const PropertyTray = () => {
-  return <></>;
+  return (
+    <div className="property-tray">
+      <div className="db-createProperty">
+        <h5>Properties</h5>
+        <button>+ Create Property</button>
+      </div>
+      <section className="db-operators">
+        <div className="db-operator-btns">
+          <button>All Properties</button>
+          <button>New</button>
+          <button>Editing</button>
+          <button>Published</button>
+          <button>Unpublished</button>
+        </div>
+        <div className="db-properties-filter">
+          <input type="search" placeholder="Search by name or address..." />
+        </div>
+      </section>
+      <section className="db-display-properties"></section>
+    </div>
+  );
 };
 export default PropertyTray;
