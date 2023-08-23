dc978e6d595c8a67649b714a722b54d5c02ccd50
Oladapo Olusola <Oladapo.O@bullseye.local>
2023-08-23 16:59:10 +0100

    updates

--- Client/campuspro-app/src/RenderSignUp.js
+++ Client/campuspro-app/src/RenderSignUp.js
@@ -10,149 +10,57 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "./images/campuspro(6).png";
-import Joi from "joi";
-// import { toast } from "react-toastify";
-// import { Link } from "react-router-dom"; 
-import Login from "./Components/Login";
-// import Modal from "react-modal";
 
 // import gold from './gold.png'
 // Modal.setAppElement("#root");
 const Signup = () => {
-  const [inputValues, setInputValues] = useState({
-    firstName: "",
-    lastName: "",
-    Phone: "",
-    Email: "",
-    Password: "",
-    confirmPassword: "",
-    userType: "",
-  });
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [error, setErrors] = useState({});
-  const [checked, setChecked] = useState(false);
-  const [buttonChecked, setBtnChecked] = useState("");
+  const [message, setMsg] = useState();
 
-  const Schemas2 = {
-    firstName: Joi.string().required().trim().messages({
-      "string.base": `First name should be a text`,
-      "string.empty": `Firstname cannot be empty`,
-      "any.required": `Firstname field is required`,
-    }),
-    lastName: Joi.string().required().trim().messages({
-      "string.base": `Lastname should be a text`,
-      "string.empty": `Lastname cannot be empty`,
-      "any.required": `Lastname field is required`,
-    }),
-    Phone: Joi.string()
-      .trim()
-      .required()
-      .pattern(new RegExp(/^((\+234)+|0)[7-9]{1}[0-9]{9}$/))
-      .messages({
-        "string.pattern.base": `Invalid phone number`,
-        "any.required": "phone Number must not be empty",
-      }),
-
-    Email: Joi.string()
-      .trim()
-      .email({ tlds: { allow: false } })
-      .required()
-      .messages({
-        "string.email": `Invalid email: 'example@gmail.com'`,
-        "any.required": `Email is required`,
-        "string.empty": `Email cannot be empty`,
-      }),
-    Password: Joi.string()
-      .required()
-      .min(8)
-      .pattern(
-        new RegExp(/(?=.*[A-Z])[a-zA-Z0-9]+[\#\@\$\%\&\*\(\)\>\<\~\{\}]+/)
-      )
-      .messages({
-        "string.pattern.base": `Password must contain atleast one capital letter and one special characters`,
-        "any.required": `Password field is required`,
-        "string.min": `Password must be minimum 8 characters`,
-      }),
-
-    confirmPassword: Joi.any().valid(Joi.ref("Password")).required().messages({
-      "string.required": "Confirm Password is required",
-      "any.only": "Passwords do not match",
-      "password.confirm": `Your passwords no gree`,
-    }),
-
-    userType: Joi.string().required().trim().messages({
-      "string.base": `Select your type of user`,
-      "string.empty": `You must select type of user`,
-      "any.required": `user type is required`,
-    }),
-  };
-  // const confirmPwd = (e) => {
-  //   const { name, value } = e.target;
-
-  //   e.preventDefault();
-  //   if (password !== PASSWORD) {
-  //   }
-  // };
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isSignUpSuccessful, setIsSignUpSuccessful] = useState(false);
 
   const openModal = () => {
-    setIsModalOpen(true);
+    setModalIsOpen(true);
   };
 
+  const navigator = useNavigate();
   const closeModal = () => {
-    setIsModalOpen(false);
+    setModalIsOpen(false);
+
+    navigator("/login");
   };
 
-  const validateProperty = (event) => {
-    const { name, value } = event.target;
-    const obj = { [name]: value };
-    const subSchema = Joi.object({ [name]: Schemas2[name] });
-    const result = subSchema.validate(obj);
-    const { error } = result;
-    return error ? error.details[0].message : null;
-  };
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    let errorData = { ...error };
-    const errorMessage = validateProperty(event);
-    if (errorMessage) {
-      errorData[name] = errorMessage;
-      // !checked && setChecked((prev) => prev);
-    } else {
-      delete errorData[name];
-    }
-    let userData = { ...inputValues };
-    userData[name] = value;
-    setInputValues(userData);
-    setErrors(errorData);
-    console.log(inputValues);
-
-    console.log(Object.values(inputValues).length);
-  };
-
-  const handleCheckedButton = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    let userData = { ...inputValues };
-    userData[name] = value;
-    setInputValues(userData);
-    // setInputValues((preValues) => ({
-    //   ...preValues,
-    //   [name]: value,
-    // }));
-    console.log(Object.values(inputValues).length);
-    console.log(inputValues);
-
-    !checked && setChecked((prev) => !prev);
-  };
-
-  const resetCheckedButton = (e) => {
-    e.preventDefault();
-    setBtnChecked(null);
-  };
+  const validationSchema = yup.object({
+    firstName: yup.string().required(" First name is required"),
+    lastName: yup.string().required(" Last name is required"),
+    Phone: yup
+      .string()
+      .required(" Phone number is required")
+      .matches(/^((\+234)+|0)[7-9]{1}[0-9]{9}$/, "Invalid phone number"),
+    Email: yup
+      .string()
+      .email("Invalid email: 'example@gmail.com'")
+      .required(" Price is required"),
+    userType: yup
+      .string()
+      .oneOf(["Agent", "Merchant"], "Select a valid user type")
+      .required("User type is required"),
+    Password: yup
+      .string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+        "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
+      ),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("Password"), null], "Passwords must match")
+      .required("Confirm Password is required"),
+  });
 
   const formik = useFormik({
-    initialValues:{
+    initialValues: {
       firstName: "",
       lastName: "",
       Phone: "",
